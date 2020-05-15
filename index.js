850c3dacc500b0ae042ba410d33aa37cdb2961fe
Kristian Hestetun <kristian.hestetun@kantega.no>
2020-05-15 14:43:08 +0200

    Add metrics from express-prom-bundle

--- index.js
+++ index.js
@@ -1,32 +1,50 @@
 const express = require('express');
-const swStats = require('swagger-stats');
+const swaggerStats = require('swagger-stats');
+const promBundle = require('express-prom-bundle');
+const promBundleMetrics = promBundle({
+    includeMethod: true, 
+    includePath: true,
+    includeUp: true,
+    metricsPath: '/prom-bundle/metrics',
+    customLabels: { applicaton: 'app-with-metrics' },
+});
 
 const port = 3000;
 const app = express();
-app.use(swStats.getMiddleware( {name: 'swagger-stats-sandbox'} ));
+app.use(swaggerStats.getMiddleware( {name: 'app-with-metrics'} ));
+app.use(promBundleMetrics);
 
 const links = 
-            `<ul>
-                <li><a href="/good-service-1">/good-service-1</a></li>
-                <li><a href="/good-service-2">/good-service-2</a></li>
-                <li><a href="/bad-service">/bad-service</a></li>
-                <li><a href="/unstable-service">/unstable-service</a></li>
-            </ul>`;
+`<ul>
+    <li><a href="/good-service">/good-service</a></li>
+    <li><a href="/bad-service">/bad-service</a></li>
+    <li><a href="/unstable-service">/unstable-service</a></li>
+    <li><a href="/slow-service">/slow-service</a></li>
+</ul>
+<h3>express-prom-bundle</h3>
+<ul><li><a href="/prom-bundle/metrics">/prom-bundle/metrics</a></li></ul>
+<h3>swagger-stats</h3>
+<ul>
+    <li><a href="/swagger-stats/metrics">/swagger-stats-metrics</a></li>
+    <li><a href="/swagger-stats/ux">/swagger-stats-ux</a></li>
+</ul>`;
 
 const replySuccess = (req, res) => res.status(200).send(`OK\n${links}`);
 const replyError = (req, res) => res.status(500).send(`Server error\n${links}`);
 const replyRandomly = (errorRate) => (req, res) => {
     if (Math.random() <= errorRate)
-        return replyError(req, res);
+    return replyError(req, res);
     return replySuccess(req, res);
 }
+const replyLate = delay => replyFunc => {
+    setTimeout(() => replyFunc, delay);
+};
 
-app.get('/', (req, res) => res.send('Hello world\n' + links));
-app.get('/good-service-1', replySuccess);
-app.get('/good-service-2', replySuccess);
+app.get('/', (req, res) => res.send('app-with-metrics\n' + links));
+app.get('/good-service', replySuccess);
 app.get('/bad-service', replyError);
 app.get('/unstable-service', replyRandomly(0.5));
-app.get('/unknown-service', (req, res) => res.status(404).send('Unknown service not found'));
+app.get('/slow-service', replyLate(1000)(replyRandomly(0.3)));
 
 app.listen(port, () => 
-console.log(`App listening at http://localhost:${port}.\nStats at http://localhost:${port}/swagger-stats/ux`));
+console.log(`App listening at http://localhost:${port}.`));
