a5081285560d1ff0313440c31c1cef5f9bb45da7
Kristian Hestetun <kristian.hestetun@kantega.no>
2020-05-15 08:46:18 +0200

    Render links on all endpoints

--- index.js
+++ index.js
@@ -5,15 +5,27 @@ const port = 3000;
 const app = express();
 app.use(swStats.getMiddleware( {name: 'swagger-stats-sandbox'} ));
 
-app.get('/', (req, res) => res.send('Hello world'));
-app.get('/good-service-1', (req, res) => res.status(200).send('Successful response from good service'));
-app.get('/good-service-2', (req, res) => res.status(200).send('Successful response from good service 2'));
-app.get('/bad-service', (req, res) => res.status(500).send('Error on bad service'));
-app.get('/unstable-service', (req, res) => {
-    if (Math.random() > 0.5)
-        return res.sendStatus(500);
-    return res.sendStatus(200);
-});
+const links = 
+            `<ul>
+                <li><a href="/good-service-1">/good-service-1</a></li>
+                <li><a href="/good-service-2">/good-service-2</a></li>
+                <li><a href="/bad-service">/bad-service</a></li>
+                <li><a href="/unstable-service">/unstable-service</a></li>
+            </ul>`;
+
+const replySuccess = (req, res) => res.status(200).send(`OK\n${links}`);
+const replyError = (req, res) => res.status(500).send(`Server error\n${links}`);
+const replyRandomly = (errorRate) => (req, res) => {
+    if (Math.random() <= errorRate)
+        return replyError(req, res);
+    return replySuccess(req, res);
+}
+
+app.get('/', (req, res) => res.send('Hello world\n' + links));
+app.get('/good-service-1', replySuccess);
+app.get('/good-service-2', replySuccess);
+app.get('/bad-service', replyError);
+app.get('/unstable-service', replyRandomly(0.5));
 app.get('/unknown-service', (req, res) => res.status(404).send('Unknown service not found'));
 
 app.listen(port, () => 
